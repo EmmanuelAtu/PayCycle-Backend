56e3e632d3e2e6f7d5c7a35f1e7276c5feee2ee0
EmmanuelAtu <emmanuelatujuniourtun@gmail.com>
2026-07-06 17:05:38 +0100

    added wallet.router

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import model
 from database import engine
-from routers import auth , plans, customer,suscription,webhook,dashboard, join
+from routers import auth , plans, customer,suscription,webhook,dashboard, join , wallet
 import httpx
 from services.nomba_client import nomba
 from core.config import settings
@@ -85,3 +85,4 @@ app.include_router(customer.router)
 app.include_router(suscription.router)
 app.include_router(webhook.router)
 app.include_router(join.router)
+app.include_router(wallet.router)
